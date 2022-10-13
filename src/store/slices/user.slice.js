de14c681f056499ef8e0dc04de644a1dc69a21af
FlorencioQuelca <florens.lonwe@gmail.com>
2022-10-12 21:20:39 -0400

    plantilla redux

---
+++ src/store/slices/user.slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: 'nameUser',
+    initialState: '',
+    //actions del stado
+    reducers: {
+
+    }
+})
+
+export default userSlice.reducer // representativo
